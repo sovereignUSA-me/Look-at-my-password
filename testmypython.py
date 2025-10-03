91a3fc90fa3ce0cbfc280b3f1d2a001fb003818e
sovereignUSA-me <francemaison09@gmail.com>
2025-10-02 18:45:17 -0700

    Create testmypython.py

---
+++ testmypython.py
@@ -0,0 +1,7 @@
+print("welcome")
+password = "topsecretpassword1234"
+password = input("Please enter the password: ")
+if password == "topsecretpassword1234":
+    print("Access granted")
+else:
+    print("Access denied")
